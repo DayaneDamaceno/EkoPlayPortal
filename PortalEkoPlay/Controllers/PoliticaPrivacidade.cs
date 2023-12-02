6a3f1e9fd4ae21fde81f7e3aec787639c8014ef1
Dayane Damaceno <Dayane.Damaceno@t-systems.com>
2023-12-02 15:17:28 -0300

    Politica de privacidade

---
+++ PortalEkoPlay/Controllers/PoliticaPrivacidade.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PortalEkoPlay.Controllers;
+
+public class PoliticaPrivacidade : Controller
+{
+	
+	public IActionResult Index()
+	{
+		return View();
+	}
+}
